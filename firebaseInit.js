d8682af7d5ce619c369b6aca906014c743103542
watsayot <watsayot.p@kkumail.com>
2024-03-23 08:46:41 +0700

    adding dist

--- firebaseInit.js
+++
@@ -1,8 +0,0 @@
-// firebaseInit.js
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "./src/firebaseConfig";
-
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
